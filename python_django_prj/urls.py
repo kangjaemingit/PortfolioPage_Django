e2e5eb6d3cf667bd0cb5a44975838170129986eb
kangjaemingit <71342315+kangjaemingit@users.noreply.github.com>
2021-11-10 16:23:43 +0900

    외부 라이브러리 사용 크리스피 폼, 마크다운, 구아로

--- python_django_prj/urls.py
+++ python_django_prj/urls.py
@@ -22,7 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('markdownx/', include('markdownx.urls')),
+    path('account/', include('allauth.urls')),
     path('', include('single_pages.urls')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
